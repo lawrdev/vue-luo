b72c65471553a8042ef5fede8f242db9cf21ab9e
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-08-28 14:39:43 +0100

    feat: test case

---
+++ src/components/TestCase.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="btn-wrapper">
+    <button
+      @click="
+        () => {
+          $emit('counterEvent', 10)
+        }
+      "
+    >
+      Do something cool
+    </button>
+  </div>
+
+  <div>{{ count }}</div>
+</template>
