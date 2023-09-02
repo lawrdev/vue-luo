a1b67f87f126645125c69739b319fbcf08c30a97
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-09-02 22:30:04 +0100

    chore: types

---
+++ src/util/types.ts
@@ -0,0 +1,12 @@
+export interface PostType {
+  body: string
+  id: number
+  title: string
+  userId: number
+}
+export type PostTypeArray = {
+  body: string
+  id: number
+  title: string
+  userId: number
+}[]
